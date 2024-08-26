8be74abf7a4b0a2b33068fa4882505731fc09ba1
KallkaGo <765830637@qq.com>
2024-08-26 13:12:59 +0800

    fix: progress error

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,19 +1,31 @@
-import { Html,useProgress } from "@react-three/drei"
+import { Html, useProgress } from "@react-three/drei"
+import { useEffect, useState } from "react"
 
 
 
 const Loader = () => {
-  const {progress} = useProgress()
+
+  const totalLoad = 67
+
+  const { loaded } = useProgress()
+
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    setProgress(loaded / totalLoad)
+  }, [loaded])
+
+
   return (
-   <Html center >
+    <Html center >
       <span className="canvas-load whitespace-nowrap text-[16px] ">Loading : </span>
       <p style={{
-        fontSize:14,
-        color:'#f1f1f1',
-        fontWeight:800,
-        display:"inline-block"
-      }}>{progress.toFixed(2)}</p>
-   </Html>
+        fontSize: 14,
+        color: '#f1f1f1',
+        fontWeight: 800,
+        display: "inline-block"
+      }}>{`${(progress * 100).toFixed(2)}%`}</p>
+    </Html>
   )
 }
 
