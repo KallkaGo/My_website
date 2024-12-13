04294d681c39afe8ac46017a02e2c112b289dc1c
More <765830637@qq.com>
2024-12-13 16:53:26 +0800

    fix: the timing of component animation in mobile projects is wrong

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,15 +1,19 @@
 import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { staggerContainer } from '../utils/motion'
+import { getSystem } from '../utils/getSystem'
 
 const SectionWrapper = (Component, idName) =>
   function HighOrderComponent () {
+
+    const flag = getSystem() === 'pc'
+    
     return (
       <motion.section
         variants={staggerContainer()}
         initial='hidden'
         whileInView={'show'}
-        viewport={{ once: true, amount: 0.1 }}
+        viewport={{ once: true, amount: flag ? 0.1 :'some' }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
         <span className='hash-span' id={idName}>
